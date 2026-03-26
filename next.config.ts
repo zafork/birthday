d0fdae65e7bcc731e1e48cc7c1c6475e0be6e9e8
zafork <aroni_ar@hotmail.com>
2026-03-26 01:09:49 -0500

    fix: Corrigiendo rutas

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/birthday",
-  assetPrefix: "/birthday/",
   images: {
     unoptimized: true,
   },
